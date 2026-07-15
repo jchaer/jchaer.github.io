ca297f1d299c50638a227b8510af9a30020c27b7
Jihye Chae <chaejer@gmail.com>
2026-07-15 16:49:40 +0900

    chore: remove duplicate deployment copies

--- loop_engineering/script.js
+++
@@ -1,9 +0,0 @@
-const menuButton = document.querySelector('.menu-toggle');
-const siteNav = document.querySelector('#site-nav');
-
-if (menuButton && siteNav) {
-  menuButton.addEventListener('click', () => {
-    const isOpen = siteNav.classList.toggle('is-open');
-    menuButton.setAttribute('aria-expanded', String(isOpen));
-  });
-}
